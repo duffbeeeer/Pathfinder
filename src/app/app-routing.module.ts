432d71b02e9e3fa72fdc42c633d0263113bb3c92
Christoph Thiel <christoph.thiel@lhind.dlh.de>
2019-06-07 08:39:14 +0200

    login form commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home';
+import { AuthGuard } from './_guards';
+import { LoginComponent } from './login';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+      path: '',
+      component: HomeComponent,
+      canActivate: [AuthGuard]
+  },
+  {
+      path: 'login',
+      component: LoginComponent
+  },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { JwtInterceptor, ErrorInterceptor, fakeBackendProvider } from './_helpers';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+      BrowserModule,
+      ReactiveFormsModule,
+      HttpClientModule,
+      AppRoutingModule
   ],
-  providers: [],
+  declarations: [
+      AppComponent,
+      HomeComponent,
+      LoginComponent
+  ],
+  providers: [
+      { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+      { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+      // provider used to create fake backend
+      fakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

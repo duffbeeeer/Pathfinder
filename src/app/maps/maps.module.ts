fc5e424ac330c96dc29fa1ff0a02d11989279f9f
Christoph Thiel <christoph.thiel@lhind.dlh.de>
2019-06-26 10:10:06 +0200

    integrated angular directions api

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MapsComponent } from './maps.component';
 import { AgmCoreModule } from '@agm/core';
+import { AgmDirectionModule } from 'agm-direction';
 
 @NgModule({
   imports: [
     CommonModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyC76G02b0O-c2AJhsX9nmcn06_0clEcNUM'
-    })
+    }),
+    AgmDirectionModule
   ],
   declarations: [
     MapsComponent

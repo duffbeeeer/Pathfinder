4f8788bd1e3fde5e53c350291de60c94273499ec
Jan Friese <jfriese82@gmail.com>
2019-08-02 10:29:39 +0200

    wip

--- src/app/augmented/augmented.component.ts
+++ src/app/augmented/augmented.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef, ViewContainerRef, ViewChild,  } from '@angular/core';
 import { WebcamInitError, WebcamUtil } from 'ngx-webcam';
 import { Subject, Observable } from 'rxjs';
+import { element } from '@angular/core/src/render3';
 require('aframe-event-set-component');
 declare var require: any;
 @Component({
@@ -14,10 +15,18 @@ export class AugmentedComponent implements OnInit {
   public screenWidth: number;
   public screenHeight: number;
   public multipleWebcamsAvailable = false;
-  public newPosition = "0.002 0.002 0.002";
-  constructor() { }
-
+  newX: number;
+  newY: number;
+  newZ: number;
+  height:number = 0.5;
+  aframed = (window as any).AFRAME;
+  
+  constructor(private hostElement:ViewContainerRef) { }
+  
+  @ViewChild('coinlock') coinBlock;
+  
   ngOnInit() {
+    console.log(this.aframed);
     WebcamUtil.getAvailableVideoInputs()
       .then((mediaDevices: MediaDeviceInfo[]) => {
         this.multipleWebcamsAvailable = mediaDevices && mediaDevices.length > 1;
@@ -25,8 +34,42 @@ export class AugmentedComponent implements OnInit {
     this.screenWidth = window.innerWidth;
     this.screenHeight = window.innerHeight - 90;
     console.log("Screen Width: " + this.screenWidth + " Screen Height: " + this.screenHeight);
-  }
+    
+    
+    
+    this.aframed.registerComponent('cursor-listener', {
+      init: function () {
+        var lastIndex = -1;
+        var COLORS = ['red', 'green', 'blue'];
+        this.el.addEventListener('click', function (evt) {
+          lastIndex = (lastIndex + 1) % COLORS.length;
+          this.object3D.position.set(0, 0.5, 0);
+          console.log('I was clicked at: ', evt.detail.intersection.point);
+        });
+      }
+    });
 
+    
+    this.aframed.registerComponent('elem-click', {
+      init: function () {
+          const el = this.el;
+          el.addEventListener('click', function (evt) {
+              console.log('Hi!')
+          });
+      }
+    });
+  }
+  public func(){
+    // this.newX = Math.floor(Math.random() * Math.floor(1))+1
+    // this.newY = Math.floor(Math.random() * Math.floor(1))+1
+    // this.newZ = Math.floor(Math.random() * Math.floor(1))+1
+    // this.coinBlock.nativeElement.object3D.position.set(1,2,3);
+    // console.log(this.coinBlock.nativeElement.object3D);
+    // console.log(this.cursor.nativeElement.object3D.el.attributes);
+    // console.log(this.aframe.nativeElement.components.raycaster.refreshObjects());
+    this.height-=0.3;
+    // this.aframe.nativeElement.components.raycaster.refreshObjects();
+  }
   public handleInitError(error: WebcamInitError): void {
     if (error.mediaStreamError && error.mediaStreamError.name === "NotAllowedError") {
       console.warn("Camera access was not allowed by user!");
--- src/app/augmented/augmented.module.ts
+++ src/app/augmented/augmented.module.ts
@@ -2,12 +2,14 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AugmentedComponent } from './augmented.component';
 import { WebcamModule } from 'ngx-webcam';
+import { AframePipeModule } from 'angular-aframe-pipe';
 
 @NgModule({
   declarations: [AugmentedComponent],
   imports: [
     CommonModule,
-    WebcamModule
+    WebcamModule,
+    AframePipeModule
   ],
   exports: [AugmentedComponent],
   schemas:[CUSTOM_ELEMENTS_SCHEMA]
